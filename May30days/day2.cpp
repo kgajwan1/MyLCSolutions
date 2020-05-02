212217234892bc65870157390f9d392814e53990
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2020-05-02 08:23:49 -0400

    Create day2.cpp

---
+++ May30days/day2.cpp
@@ -0,0 +1,18 @@
+//Jewels and stones
+
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        unordered_map<char, int> jewels;
+        for(int i=0; i<J.length(); i++){
+            jewels[J[i]]++;
+        } 
+        int res = 0;
+        for(int i=0; i<S.length(); i++){
+            if(jewels.count(S[i])){
+                res++;
+            }
+        }
+        return res;
+    }
+};
