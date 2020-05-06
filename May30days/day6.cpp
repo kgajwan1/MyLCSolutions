4dde9f80f7f80f5655365daba14c087f9e630298
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2020-05-06 10:45:48 -0400

    Create day6.cpp

---
+++ May30days/day6.cpp
@@ -0,0 +1,23 @@
+//majority element
+//I could use moorse voting algo, but that is for another day
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> map(26);
+        for (auto c:tasks)
+        {
+            map[c-'A']++;
+        }
+        sort(map.begin(),map.end());
+        
+        int max_val = map[25]-1;
+        int idle_slots = max_val*n;
+        for (int i = 24;i>=0 && map[i]>0;i--)
+        {
+            idle_slots-=min(map[i],max_val);
+        }
+        return idle_slots > 0 ? idle_slots + tasks.size() : tasks.size();
+    }
+};
+
